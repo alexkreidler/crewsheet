3c971428df7b657c86dae3ffbcb25a0444e7d101
Timothy Higinbottom <timhigins@gmail.com>
2016-10-22 16:15:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
-var dynamodb = new AWS.DynamoDB();
-
+db = firebase.database();
 function e(x) {
     return document.getElementById(x);
 }
 
 var submit = e('submit');
 submit.addEventListener('click', function(){
-    var params = {
-      Item: { /* required */
-        email: e('email').value,
-        password: e('password').value
-        /* anotherKey: ... */
-      },
-      TableName: 'crewsheet-users', /* required */
-    };
-    dynamodb.putItem(params, function(err, data) {
-      if (err) console.log(err, err.stack); // an error occurred
-      else     console.log(data);           // successful response
-    });
+    database.child('users').push({'email': e('email')});
 });
